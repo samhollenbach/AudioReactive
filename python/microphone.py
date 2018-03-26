e34dda92a7601538c0ab25852a0c5d97913d52f4
Sam Hollenbach <shollenb@macalester.edu>
2018-03-26 00:58:33 -0500

    output test

--- python/microphone.py
+++ python/microphone.py
@@ -11,6 +11,7 @@ def start_stream(callback):
                     channels=1,
                     rate=config.MIC_RATE,
                     input=True,
+                    output = 1,
                     frames_per_buffer=frames_per_buffer)
     overflows = 0
     prev_ovf_time = time.time()
