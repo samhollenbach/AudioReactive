91ae270139097552ddc9b9d99b5fe6d911f194a4
Sam Hollenbach <shollenb@macalester.edu>
2018-03-30 19:09:26 -0500

    exception on overflow?

--- python/microphone.py
+++ python/microphone.py
@@ -11,7 +11,8 @@ def start_stream(callback):
                     channels=1,
                     rate=config.MIC_RATE,
                     input=True,
-                    frames_per_buffer=frames_per_buffer)
+                    frames_per_buffer=frames_per_buffer,
+                    exception_on_overflow=False)
     overflows = 0
     prev_ovf_time = time.time()
     while True:
